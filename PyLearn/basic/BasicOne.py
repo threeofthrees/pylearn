2e8dd97fbdbd8bb5f7f8680a04672573daf5a4a2
threeofthrees <m.s.rahman33@hotmail.com>
2016-09-24 00:43:29 -0400

    Commit

--- PyLearn/basic/BasicOne.py
+++ PyLearn/basic/BasicOne.py
@@ -17,3 +17,13 @@ while True:  # main game loop
             sys.exit()
     pygame.display.update()
 '''
+
+listOfCars = ['Aston Martin', 'Jeep', 'Honda Accord', 'Range Rover', 'BMW 7 Series']
+listOfSchools = ['Wake Forest', 'SUNY Downstate', 'NYCOM', 'PCOM']
+listOfFootballers = ['Frank Lampard', 'Gareth Bale', 'Cristiano Ronaldo', 'Didier Drogba', 'John Terry']
+listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+listOfLists = [listOfCars, listOfSchools, listOfFootballers]
+
+print(listOfLists[1])
+print(len(listOfCars))
