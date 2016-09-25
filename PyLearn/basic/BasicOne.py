b3ba83013fd1e8952a63a051f2e50315ff7a9258
threeofthrees <m.s.rahman33@hotmail.com>
2016-09-24 22:31:50 -0400

    Commit

--- PyLearn/basic/BasicOne.py
+++ PyLearn/basic/BasicOne.py
@@ -5,7 +5,7 @@ Created on Sep 23, 2016
 '''
 import pygame, sys
 from pygame.locals import *
-'''
+
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World')
@@ -16,14 +16,3 @@ while True:  # main game loop
             pygame.quit()
             sys.exit()
     pygame.display.update()
-'''
-
-listOfCars = ['Aston Martin', 'Jeep', 'Honda Accord', 'Range Rover', 'BMW 7 Series']
-listOfSchools = ['Wake Forest', 'SUNY Downstate', 'NYCOM', 'PCOM']
-listOfFootballers = ['Frank Lampard', 'Gareth Bale', 'Cristiano Ronaldo', 'Didier Drogba', 'John Terry']
-listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-
-listOfLists = [listOfCars, listOfSchools, listOfFootballers]
-
-print(listOfLists[1])
-print(len(listOfCars))
